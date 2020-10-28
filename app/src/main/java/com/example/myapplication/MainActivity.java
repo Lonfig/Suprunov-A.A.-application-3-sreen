fd0aba1fed512b489b3861722973b4a15de2bb4c
Lonfig <suprunov250302@gmail.com>
2020-10-28 16:27:34 +0700

    3 'экрана'

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,34 +10,22 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
-    private Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.buttonTo2);
-        button.setOnClickListener(new View.OnClickListener(){
+        button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                openActivity2();
-            }
-        });
-
-        button2 = (Button) findViewById(R.id.buttonTo3From1);
-        button2.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                openActivity3();
+            public void onClick(View v){
+                openActivity1();
             }
         });
     }
-    public void openActivity2(){
-        Intent intent = new Intent(this, MainActivity2.class);
-        startActivity(intent);
-    }
-    public void openActivity3(){
-        Intent intent = new Intent(this, MainActivity3.class);
+    public void openActivity1() {
+        Intent intent= new Intent(this, MainActivity2.class);
         startActivity(intent);
     }
 
